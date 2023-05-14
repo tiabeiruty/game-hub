24fbeebcfc46c750551aca75e2271376b818dc23
Tia Beiruty <tiabeiruty@gmail.com>
2023-05-14 21:58:02 +0300

    Installing react query and fetching genres

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: "db1313d1e3954217a13a4d3a4f45e840",
   },
 });
