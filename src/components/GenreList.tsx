4045314b72b364cc5a375a21baeb026ab0185f16
Tia Beiruty <tiabeiruty@gmail.com>
2023-05-16 22:01:30 +0300

    Remove duplicate interfaces

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -29,7 +29,7 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
         Genres
       </Heading>
       <List>
-        {data?.results.map((genre) => (
+        {data?.results.map((genre: Genre) => (
           <ListItem key={genre.id} paddingY="5px">
             <HStack>
               <Image
