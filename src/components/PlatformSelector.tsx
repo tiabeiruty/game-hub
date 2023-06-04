8939bb19c05f7a0a87eba45ac1695614c4d22abf
Tia Beiruty <tiabeiruty@gmail.com>
2023-06-04 21:52:29 +0300

    Move entity interfaces

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,6 +1,6 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
-import { Platform } from "../hooks/usePlatforms";
+import { Platform } from "../entities/Platform";
 import usePlatforms from "../hooks/usePlatforms";
 import usePlatform from "../hooks/usePlatform";
 import useGameQueryStore from "../services/store";
