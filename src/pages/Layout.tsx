ca3a27f13ec116bc99354081e8e446aacceb0245
Tia Beiruty <tiabeiruty@gmail.com>
2023-06-04 21:42:37 +0300

    Fetch game details

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,10 +1,13 @@
+import { Box } from "@chakra-ui/react";
 import NavBar from "../components/NavBar";
 import { Outlet } from "react-router-dom";
 const Layout = () => {
   return (
     <>
       <NavBar />
-      <Outlet />
+      <Box padding={5}>
+        <Outlet />
+      </Box>
     </>
   );
 };
